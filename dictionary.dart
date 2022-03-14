01d0386b054fe5e3a33b5a1d5b3a22f55d8b03dd
Abdul haseeb imran <98330141+Abdulhaseebimran@users.noreply.github.com>
2022-03-14 12:36:51 +0500

    dart_prac_dictionary_list.

---
+++ dictionary.dart
@@ -0,0 +1,34 @@
+void main() {
+  // var map1 = {1: "Hasseb", 2: "Abdullah", 3: "Musaib"};
+  // print(map1.keys);
+  // print(map1.values);
+  // print(map1.length);
+  // var map1 = {};
+  // print(map1.isNotEmpty);
+  // var map1 = {1: "Hasseb", 2: "Abdullah", 3: "Musaib"};
+  // map1[4] = "Ali";
+  // print(map1);
+  // var map1 = {1: "Hasseb", 2: "Abdullah", 3: "Musaib", 4: "Lahore"};
+  // map1.addAll({6: "Karachi", 7: "Islamabad", 8: "Peshawar"});
+  // // var x = map1.putIfAbsent(5, () => "Manchester");
+  // print(map1);
+  // var map1 = {1: "Hasseb", 2: "Abdullah", 3: "Musaib"};
+  // map1.remove(3);
+  // print(map1);
+  // var map1 = {1: "Hasseb", 2: "Abdullah", 3: "Musaib"};
+  // map1.removeWhere((key, value) => value.startsWith("H"));
+  // print(map1);
+  // var map1 = {1: "Hasseb", 2: "Abdullah", 3: "Musaib", 4: "Lahore"};
+  // var map2 = {5: "Peshawar", 6: "Garden west"};
+  // var mapp = {}
+  //   ..addAll(map1)
+  //   ..addAll(map2);
+  // print(mapp);
+  // var list1 = [1, 2, 3];
+  // var list2 = ["haseeb", "abdullah", "musaib"];
+  // var mapp = Map.fromIterables(list1, list2);
+  // print(mapp);
+  var map1 =[1:"haseeb", 2:"abdullah", 3:"musaib"];
+  var ab = map1.containsvalues("Bilal");
+  print(ab);
+}
---
+++ list.dart
@@ -0,0 +1,45 @@
+void main() {
+  // var list1 = ["Haseeb", 19, 44.5, "true"];
+  // var list2 = [1, 2, 4, 55, 66, 77, 99];
+  // var list3 = ["Abdul haseeb", "Abdullah", "Musaib"];
+  // var list4 = [];
+  // if (list4.isEmpty) {
+  //   print("This list is  Empty!");
+  // }
+  // list4.add("Haseeb");
+  // list4.add("Abd ");
+  // list4.add("usman");
+  // list4.add(44);
+  // list4.add(true);
+  // list4.add(77.009);
+  // print(list4);
+  // if (list4.isNotEmpty) {
+  //   print("List is not Empty");
+  // }
+  // list4.clear();
+  // print(list4);
+  // list3.sort();
+  // print(list3);
+
+  // list1[2] = "Musiab";
+  // list1.replaceRange(0, 2, ["ABDULLAH", "false", 90]);
+  // print(list1.length);
+  // print(list1.first);
+  // print(list1.last);
+  // print(list1.elementAt(2));
+  //  list2.sort();
+  // print(list1);
+  // var list7 = [1, 4, 5, 6, 6, 90, 989, 1000];
+  // var x = List.of(list7.reversed);
+  // print(x);
+  var list8 = [23, 45, 56, 78, 89];
+  // list8.removeWhere((element) => (element) < 0);
+  list8.removeRange(2, 34);
+  print(list8);
+  // list7.addAll(list8);
+  // print(list7);
+  // list7.sort();
+  // print(list7);
+  // list8.insert(3, 34);
+  // print(list8);
+}
